0cdacf9ae3e6a5c5da807854f08a02c079b44a4d
Otagiri-Y <otagiri.yo.hey@gmail.com>
2025-02-03 23:03:27 +0900

    デプロイ状況での接続確認

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # アプリケーションのインスタンスを作成
 app = FastAPI()
 
+frontend_origin = os.getenv("FRONTEND_ORIGIN", "http://localhost:3000")
+
 # CORS設定
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # 許可するオリジン
+    allow_origins=[frontend_origin],  # 許可するオリジン
     allow_credentials=True,
     allow_methods=["*"],  # 許可するHTTPメソッド
     allow_headers=["*"],  # 許可するHTTPヘッダー
